d895bfbc7972a3f2258606af1ddff3927ea88302
Vsanapathi <veerababu.s@gmail.com>
2024-04-11 17:21:40 +0530

    added 2nd script ; git push origin main

---
+++ 02-variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh :: Hello Suresh, Good Morning"
+echo "Suresh :: Hello Ramesh, Good Morning" 
+echo "Ramesh :: Hw are u Suresh"
+echo "Suresh :: i am good Ramesh, How are you"
